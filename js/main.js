6a389ba4cf8f7dbd9140ada247a93bed1f8889af
Nicolas Hempe <66489169+nicolashempe@users.noreply.github.com>
2020-08-10 15:01:38 -0700

    Modified js and class names

--- js/main.js
+++ js/main.js
@@ -6,8 +6,20 @@ var cof = document.querySelector('.c');
 var tea = document.querySelector('.tea');
 
 var hide = document.querySelector('.hide');
+var hide2 = document.querySelector('.hide2');
+var hide3 = document.querySelector('.hide3');
+var hide4 = document.querySelector('.hide4');
 
 hide.addEventListener('click', function() {
+    nav.classList.toggle('open');
+});
+hide2.addEventListener('click', function() {
+    nav.classList.toggle('open');
+});
+hide3.addEventListener('click', function() {
+    nav.classList.toggle('open');
+});
+hide4.addEventListener('click', function() {
     nav.classList.toggle('open');
 });
 
