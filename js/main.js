85300ea81951a22967417efe9b451c8beb37d1ca
Nicolas Hempe <66489169+nicolashempe@users.noreply.github.com>
2020-08-10 14:34:27 -0700

    Finished Mobile optimization

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var hmenu = document.querySelector('#hmenu');
 var nav = document.querySelector('.nav-ul');
 var plus = document.querySelector('#plus');
-var cof = document.querySelector('.cof');
+var plus2 = document.querySelector('#plus2');
+var cof = document.querySelector('.c');
+var tea = document.querySelector('.tea');
+
+var hide = document.querySelector('.hide');
+
+hide.addEventListener('click', function() {
+    nav.classList.toggle('open');
+});
+
 
 hmenu.addEventListener('click', function() {
     nav.classList.toggle('open');
@@ -9,4 +18,8 @@ hmenu.addEventListener('click', function() {
 
 plus.addEventListener('click', function() {
     cof.classList.toggle('open');
+});
+
+plus2.addEventListener('click', function() {
+    tea.classList.toggle('open');
 });
