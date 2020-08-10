baf96f0aba14368b9578c7450e1e6b5a898504b8
Nicolas Hempe <66489169+nicolashempe@users.noreply.github.com>
2020-08-10 00:20:14 -0700

    Started making changes for mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var hmenu = document.querySelector('#hmenu');
 var nav = document.querySelector('.nav-ul');
+var plus = document.querySelector('#plus');
+var cof = document.querySelector('.cof');
 
 hmenu.addEventListener('click', function() {
     nav.classList.toggle('open');
+});
+
+plus.addEventListener('click', function() {
+    cof.classList.toggle('open');
 });
